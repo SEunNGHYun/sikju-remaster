9d547e2aefe181d0162608672be4f01ad2c4d48e
michael00987 <hyeongkwangseok@gmail.com>
2020-06-29 13:50:33 +0900

    🏇 svg 템플릿 작성

--- src/screens/IntroScreen.js
+++ src/screens/IntroScreen.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import {
-  View, Text, Button, StyleSheet
+  View, Image, Button, StyleSheet
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  test: {
-    fontSize: 50,
-    textAlign: 'center',
-  },
   view: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  tinyLogo: {
+    width: 400,
+    height: 600,
+  },
 });
 
 const IntroScreen = ({ navigation }) => {
   return (
     <View style={styles.view}>
-      <Text style={styles.test}>인트로 페이지</Text>
-      <Button title="3초 후" onPress={() => navigation.replace('Login')} />
+      <Image
+        style={styles.tinyLogo}
+        width="100px"
+        // eslint-disable-next-line global-require
+        source={require('../assets/images/splash.png')}
+      />
+      <Button title="3초 후" onPress={() => navigation.navigate('Login')} />
     </View>
   );
 };
