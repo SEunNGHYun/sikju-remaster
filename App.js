44a0c1fefd94a5976b239eb63d0ba38bcd910f4d
michael00987 <hyeongkwangseok@gmail.com>
2020-06-29 11:52:43 +0900

    🔧 컴포넌트 분류 파일 생성

--- App.js
+++ App.js
@@ -1,114 +1,150 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  View, Text, Button, StyleSheet
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator();
 
-const App: () => React$Node = () => {
+const styles = StyleSheet.create({
+  test: {
+    fontSize: 50,
+    textAlign: 'center',
+  },
+  view: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+});
+
+const IntroScreen = ({ navigation }) => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.view}>
+      <Text style={styles.test}>인트로 페이지</Text>
+      <Button title="3초 후" onPress={() => navigation.replace('Login')} />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const LoginScreen = ({ navigation }) => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>Login Screen</Text>
+      <Button
+        title="회원가입 버튼"
+        onPress={() => navigation.navigate('Signup')}
+      />
+      <Button
+        title="카카오 로그인"
+        onPress={() => navigation.navigate('PhoneAuth')}
+      />
+    </View>
+  );
+};
+
+const SignupScreen = () => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>Signup Screen</Text>
+      {/* <Button title="3초 후에 다음" onPress={() => navigation.navigate('Login')} /> */}
+    </View>
+  );
+};
+
+const PhoneAuthScreen = ({ navigation }) => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>PhoneAuth Screen</Text>
+      <Button
+        title="인증 후 메인으로 이동"
+        onPress={() => navigation.navigate('Tab')}
+      />
+    </View>
+  );
+};
+
+const MapScreen = () => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>지도</Text>
+      {/* <Button title="인증 후 메인으로 이동" onPress={() => navigation.navigate('Tab')} /> */}
+    </View>
+  );
+};
+
+const MainScreen = ({ navigation }) => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>메인 페이지</Text>
+      <Button title="현위치 변경" onPress={() => navigation.navigate('Map')} />
+      <Button
+        title="식당 정보"
+        onPress={() => navigation.navigate('RestaurantDetail')}
+      />
+    </View>
+  );
+};
+
+const RestaurantDetail = () => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>식당 상세 페이지</Text>
+    </View>
+  );
+};
+
+const NearRests = ({ navigation }) => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>주변식당</Text>
+      <Button
+        title="식당 정보"
+        onPress={() => navigation.navigate('RestaurantDetail')}
+      />
+    </View>
+  );
+};
+
+const MyPage = () => {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.test}>마이페이지</Text>
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="메인" component={MainScreen} />
+      <Tab.Screen name="주변" component={NearRests} />
+      <Tab.Screen name="마이페이지" component={MyPage} />
+    </Tab.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Intro"
+          component={IntroScreen}
+          options={{ title: 'titile option' }}
+        >
+          {/* {(props) => <HomeSreen {...props} extraData={someData} />} */}
+        </Stack.Screen>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="PhoneAuth" component={PhoneAuthScreen} />
+        <Stack.Screen name="Tab" component={TabNavigation} />
+        <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
